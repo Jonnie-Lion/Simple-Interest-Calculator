690d50bff1f0aed3ac1c2469281a082eed1c0f3d
Jonnie-Lion <36559049+Jonnie-Lion@users.noreply.github.com>
2021-01-31 19:54:53 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute(){    
+      
+        
+        var principal;
+        if ("principal" <= 0){
+            alert("You should entered positive numbers")
+        }
+        else{
+            principal= document.getElementById("principal").value
+        }
+
+        var rate = document.getElementById("rate").value
+        var years = 2000+ parseInt(document.getElementById("years").value)
+        var result = principal * (years-2000) * rate / 100
+        
+        document.getElementById("p1").innerHTML = "If you deposit " + "<strong>" + principal + "</strong> "  ;
+        document.getElementById("p2").innerHTML = "at an interest rate of " + "<strong>" + rate + "</strong> ";
+        document.getElementById("p3").innerHTML = "You will receive an amount of? " + "<strong>" + result + "</strong> ";
+        document.getElementById("p4").innerHTML = "in the year " + "<strong>" + years + "</strong> "; 
+
+        
+                
 }
+
+function Optionselection(select){
+    var chosen = select.options[select.selectedIndex]
+    alert("years Chosen by you is " + chosen.value); 
+}
+
+
+
         
